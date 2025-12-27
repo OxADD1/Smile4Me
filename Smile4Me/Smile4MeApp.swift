8f54d073fce4bd4cf41747c48e0efd330cbe17bb
Adrian <eberhardt.adrian@yahoo.com>
2025-12-27 22:19:17 +0100

    Initial Commit

---
+++ Smile4Me/Smile4MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smile4MeApp.swift
+//  Smile4Me
+//
+//  Created by Adrian Eberhardt on 27.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Smile4MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
