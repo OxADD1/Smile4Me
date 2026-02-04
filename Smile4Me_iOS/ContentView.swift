1adc12c0f366dc0448aee065baec8a89acb7878c
Adrian <eberhardt.adrian@yahoo.com>
2026-02-04 09:07:49 +0100

    starting iOS

---
+++ Smile4Me_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Smile4Me_iOS
+//
+//  Created by Adrian Eberhardt on 04.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Smile4Me_iOS/Smile4Me_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smile4Me_iOSApp.swift
+//  Smile4Me_iOS
+//
+//  Created by Adrian Eberhardt on 04.02.26.
+//
+
+import SwiftUI
+
+@main
+struct Smile4Me_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
