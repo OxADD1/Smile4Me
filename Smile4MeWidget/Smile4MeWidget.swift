8ce6a6d26b9a1020b7fa9ba8d4781be2198c912c
Adrian Eberhardt <eberhardt.adrian@yahoo.com>
2026-02-07 11:20:16 +0100

    starting widget & ios minimum target 18

---
+++ Smile4MeWidget/Smile4MeWidget.swift
@@ -0,0 +1,80 @@
+//
+//  Smile4MeWidget.swift
+//  Smile4MeWidget
+//
+//  Created by Adrian on 07.02.26.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), emoji: "😀")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+
+//    func relevances() async -> WidgetRelevances<Void> {
+//        // Generate a list containing the contexts this widget is relevant in.
+//    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct Smile4MeWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Time:")
+            Text(entry.date, style: .time)
+
+            Text("Emoji:")
+            Text(entry.emoji)
+        }
+    }
+}
+
+struct Smile4MeWidget: Widget {
+    let kind: String = "Smile4MeWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            
+                Smile4MeWidgetEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    Smile4MeWidget()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
---
+++ Smile4MeWidget/Smile4MeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Smile4MeWidgetBundle.swift
+//  Smile4MeWidget
+//
+//  Created by Adrian on 07.02.26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Smile4MeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Smile4MeWidget()
+    }
+}
