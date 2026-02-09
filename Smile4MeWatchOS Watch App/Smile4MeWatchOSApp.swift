b6db8a7bb8c47caa0355ce90eb5da74ff14f685f
Adrian Eberhardt <eberhardt.adrian@yahoo.com>
2026-02-09 05:49:29 +0100

    starting watchOS

---
+++ Smile4MeWatchOS Watch App/Smile4MeWatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smile4MeWatchOSApp.swift
+//  Smile4MeWatchOS Watch App
+//
+//  Created by Adrian on 09.02.26.
+//
+
+import SwiftUI
+
+@main
+struct Smile4MeWatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
